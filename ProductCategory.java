81c6b5c8d7cf2771b437a0f94e0e51310cb50d02
buildbylogic-netizen <buildbylogic@gmail.com>
2026-08-31 10:09:40 -0700

    Create ProductCategory.java

---
+++ ProductCategory.java
@@ -0,0 +1,11 @@
+package com.mycompany.smartecommercesystem.enums;
+public enum ProductCategory 
+{ 
+    ELECTRONICS, 
+    CLOTHING, 
+    BOOKS, 
+    HOME_APPLIANCES,
+    BEAUTY, 
+    SPORTS,
+    OTHER
+}
